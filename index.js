b409afb3004e1156825546ca3a20db79ed79aca9
Oleh <opavlov@conceptsoft.io>
2018-04-12 19:17:52 +0300

    0.0.1

---
+++ index.js
@@ -0,0 +1,178 @@
+const got = require('got')
+const { merge } = require('lodash')
+const tools = require('./tools')
+
+/**
+ * @typedef {object} RequestOptions
+ * @property {string} method - Request method
+ * @property {string} url - Request URL. You can pass variables with {name} notation
+ * @property {Object.<string,*>} [headers] - Additional request headers. For example you can use it for auth header
+ * @property {Object.<string,*>} [spreadInput] - Additional input properties
+ * @property {Object.<string,*>} [spreadBody] - Spread request body. You can pass variables with {name} notation
+ * @property {boolean} isFormData - If true, send body as form data
+ */
+
+/**
+ * Awesome function for creating elastic.io action, trigger or even webhook!
+ * Written by Oleh, opavlov@conceptsoft.io
+ * @param {RequestOptions} options Request options
+ */
+exports.request = function (options) {
+  return async function () {
+    try {
+      let { method, url, headers, spreadInput, spreadBody, isFormData } = options
+      let msg = arguments[0].body || {}
+      let input = merge({}, arguments[0], arguments[1], spreadInput)
+
+      let reqOptions = {}
+      reqOptions.method = method.toUpperCase()
+      reqOptions.headers = headers || {}
+      reqOptions.headers['Accept'] = 'application/json'
+
+      url = tools.parseValue(url, input, msg)
+      if (reqOptions.method === 'GET') {
+        if (url.indexOf('?') == -1) url += '?'
+        for (let prop in msg) url += `&${prop}=${msg[prop]}`
+        msg = {}
+      }
+      else {
+        if (spreadBody && spreadBody.constructor === Object) {
+          reqOptions.body = tools.parseValue(spreadBody, input, msg) 
+        }
+        
+        if (isFormData) {
+          reqOptions.form = true
+          reqOptions.body = merge(reqOptions.body, msg)
+          reqOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        } 
+        else { 
+          reqOptions.body = JSON.stringify(merge(reqOptions.body, msg))
+          reqOptions.headers['Content-Type'] = 'application/json'
+        }
+      }
+      console.log(`URL: ${url}`)
+      console.log(`REQUEST OPTIONS: ${JSON.stringify(reqOptions)}`) 
+
+      const { requestUrl, statusCode, body } = await got(url, reqOptions)
+      console.log(`RESPONSE BODY: ${body}`)
+
+      const preparedData = { 
+        requestUrl,
+        statusCode,
+        payload: tools.safeJsonParse(body) || 'NO MESSAGE'
+      }
+      if (this.emit) this.emit('data', { body: preparedData })
+      else return preparedData
+
+    } catch (error) {
+      console.log(`ERROR: ${error}`)
+      let preparedData = { error: error.message }
+      switch (error.constructor) {
+        case got.HTTPError: 
+          const { requestUrl, statusCode, body } = error.response
+          preparedData = { 
+            requestUrl,
+            statusCode, 
+            payload: tools.safeJsonParse(body) || 'NO MESSAGE' 
+          }
+          break
+        case got.RequestError: 
+          preparedData = { 
+            error: 'Invalid URL or server does not response', 
+            hostname: error.hostname
+          }
+          
+      }
+
+      if (this.emit) this.emit('data', { body: preparedData })
+      else return preparedData
+
+    } finally {
+      this.emit('end')
+    }
+  }
+}
+
+/**
+ * @typedef {object} SelectViewOptions
+ * @property {string} url - Request URL. You can pass variables with {name} notation
+ * @property {Object.<string,*>} [headers] - Additional request headers. For example you can use it for auth header
+ * @property {Object.<string,*>} [spreadInput] - Additional input properties
+ * @property {string} [schemaPath] - Path to schema in response. Use {obj.arr[0].prop} notation
+ * @property {string} valuePath - Relative path to select view option's value in schema. Use {obj.arr[0].prop} notation
+ * @property {string} titlePath - Relative path to select view option's title in schema. Use {obj.arr[0].prop} notation
+ */
+
+/**
+ * Awesome function for generating SelectView's schema
+ * Written by Oleh, opavlov@conceptsoft.io
+ * @param {SelectViewOptions} options SelectView options
+ */
+exports.genSelectViewOptions = function(options) {
+  return async function(cfg) {
+    try {
+      let { url, headers = {}, schemaPath, valuePath, titlePath } = options
+      
+      console.log(`HEADERS: ${JSON.stringify(headers)}`)
+      url = tools.parseValue(url, cfg)
+      console.log(`URL: ${url}`)
+      const body = JSON.parse((await got.get(url, { headers })).body)
+      console.log(`RESPONSE BODY: ${JSON.stringify(body)}`)
+      
+      const schema = schemaPath ? tools.deepSearch(body, schemaPath) : body
+      console.log(`SCHEMA: ${JSON.stringify(schema)}`)
+      let result = {}
+      for (obj of schema) result[tools.deepSearch(obj, valuePath)] = tools.deepSearch(obj, titlePath)
+      return result
+
+    } catch (error) {
+      console.log(`ERROR: ${JSON.stringify(error)}`)
+      throw error
+    }
+  }
+}
+
+/**
+ * @typedef {object} MetadataOptions
+ * @property {string} url - Request URL. You can pass variables with {name} notation
+ * @property {Object.<string,*>} [headers] - Additional request headers. For example you can use it for auth header
+ * @property {Object.<string,*>} [spreadInput] - Additional input properties
+ * @property {string} [schemaPath] - Path to schema in response. Use {obj.arr[0].prop} notation
+ * @property {string} namePath - Relative path to metadata field name in schema. Use {obj.arr[0].prop} notation
+ * @property {string} typePath - Relative path to metadata field type in schema. Use {obj.arr[0].prop} notation
+ * @property {string} propsPath - Relative path to metadata object-field with properties in schema. Use {obj.arr[0].prop} notation
+ * @property {string} titlePath - Relative path to metadata field title in schema. Use {obj.arr[0].prop} notation
+ * @property {string} requiredPath - Relative path to metadata <is required?> info in schema. Use {obj.arr[0].prop} notation
+ * @property {string} exclude - array of fields whitch should be exluded
+ * @property {string} wrap - name of object. It wraps all generic metadata
+ */
+/**
+ * Awesome function for generating dynamic metadata
+ * Written by Oleh, opavlov@conceptsoft.io
+ * @param {MetadataOptions} options Metadata options
+ */
+exports.genMetadata = function(options) {
+  return async function(cfg) {
+    try {
+      let { 
+        url, auth, schemaPath, namePath, typePath, 
+        propsPath, titlePath, requiredPath, exclude, wrap 
+      } = options
+      
+      let headers = {};
+      if (auth) headers = (await auth(cfg)).headers
+      url = tools.parseValue(url, cfg)
+      const body = JSON.parse((await got.get(url, { headers })).body)
+
+      const schema = schemaPath ? tools.deepSearch(body, schemaPath) : body
+      console.log(`SCHEMA: ${JSON.stringify(schema)}`)
+      return {
+        in: tools.schemaToMetadata(schema, namePath, typePath, propsPath, titlePath, requiredPath, exclude, wrap)
+      }
+
+    } catch (error) {
+      console.log(`ERROR: ${JSON.stringify(error)}`)
+      throw error
+    }
+  }
+}
---
+++ tools/index.js
@@ -0,0 +1,110 @@
+const safeJsonParse = value => {
+  try {
+    return JSON.parse(value)
+  } catch (error) {
+    return value
+  }
+}
+
+const deepSearch = (obj, path) => {
+  try {
+    const steps = path.split('.')
+    let current = obj
+    for (let i = 0; i < steps.length; i++) {
+      const [ name, index ] = steps[i].split(/[\[\]]/g)
+      current = index ? current[name][index] : current[name]
+    }
+    return current
+  }
+  catch (err) {
+    return undefined
+  }
+}
+
+const parseValue = (schema, input, flowData = {}) => {
+  try {
+    let output = {}
+
+    switch (typeof schema) {
+      case 'string':
+        const match = schema.match(/\{\{\w+?\}\}/)
+        if (match) {
+          const name = match[0].substring(2, schema.length - 2)
+          output = input[name] || null
+          if (output && flowData[name]) delete flowData[name]
+        }
+        else {
+          const value = schema.replace(/\{\w+?\}/g, sub => {
+            const prop = sub.substring(1, sub.length - 1)
+            if (input[prop]) {
+              if (flowData[prop]) delete flowData[prop]
+              return input[prop]
+            } 
+            else return ""
+          })
+          output = value || null
+        }
+        break
+      
+      case 'object':
+        if (schema.constructor === Array) output = []
+        for (let i in schema) { 
+          const value = parseValue(schema[i], input, flowData)
+          if (value) output[i] = value
+        }
+    }
+
+    return output
+
+  } catch (error) {
+    console.log(`ERROR: ${error}`)
+    throw error
+  }
+}
+
+const schemaToMetadata = (schema, namePath, typePath, propsPath, titlePath, requiredPath, exclude = [], wrap) => {
+  try {
+    let metadata = {}
+    
+    for (let i in schema) { 
+      const name = deepSearch(schema[i], namePath)
+      const type = deepSearch(schema[i], typePath)
+      const props = deepSearch(schema[i], propsPath)
+      
+      if (exclude.indexOf(name) === -1) {
+        metadata[name] = {}
+        if (props) {
+          metadata[name] 
+            = schemaToMetadata(props, namePath, typePath, propsPath, titlePath, requiredPath, exclude)
+        }
+        else metadata[name] = {
+          type: type === 'string' || 'boolean' || 'number' || 'object' ? type : 'string',
+          title: deepSearch(schema[i], titlePath) || name,
+          required: deepSearch(schema[i], requiredPath) || false
+        }
+      }
+    }
+    if (typeof wrap === 'string') return {
+      type: 'object',
+      properties: {
+        [wrap]: {
+          type: 'object',
+          title: wrap,
+          properties: metadata
+        }
+      }
+    }
+    else return {
+      type: 'object',
+      properties: metadata
+    }
+
+  } catch (error) {
+    console.log(`ERROR: ${JSON.stringify(error)}`)
+    throw error
+  }
+}
+
+module.exports = {
+  safeJsonParse, deepSearch, parseValue, schemaToMetadata
+}
